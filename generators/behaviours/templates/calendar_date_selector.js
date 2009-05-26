18257b4420947406bd51b38e2c722d96fbbcc1a4
dwg <arni.dwg@gmail.com>
2009-05-26 00:36:20 -0700

    code cleanup

--- generators/behaviours/templates/calendar_date_selector.js
+++ generators/behaviours/templates/calendar_date_selector.js
@@ -1,18 +1,17 @@
 CalendarDateSelector = Behavior.create({
 	initialize: function(options) {
 		this.options = $H(options);
-		this.target_element = this.element;
 		
 		var image_path = this.options.unset('image') || '/images/calendar_date_select/calendar.gif';
-		if (!this.options.get('embedded')) {
-			var image = $img({src: image_path, 'class': 'calendar_date_select_popup_icon', style: 'border: 0px; cursor: pointer;'})
-			Event.observe(image, 'click', function(event) {
-				new CalendarDateSelect(this.target_element, this.options);
-				event.stop();
-			}.bind(this));
-			this.target_element.insert({after: image});
-		} else {
+		if (this.options.get('embedded')) {
 			new CalendarDateSelect(this.target_element, this.options);
+		} else {
+			var image = $img({src: image_path, className: 'calendar_date_select_popup_icon'}).setStyle({border: 'none', cursor: 'pointer'});
+			image.observe('click', function(event) {
+				new CalendarDateSelect(this.element, this.options);
+				event.stop();
+			}.bindAsEventListener(this));
+			this.element.insert({after: image});
 		}
 	}
 });
